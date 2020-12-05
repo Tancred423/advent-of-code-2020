0b89a51b3f6fabe5d8cf6ca2af9b37c2e63855c9
Tancred423 <thonkred@gmail.com>
2020-12-05 14:02:35 +0100

    Day 5

---
+++ d5p2.py
@@ -0,0 +1,40 @@
+from puzzle_input import lines
+import math
+
+seat_ids = []
+
+for line in lines:
+    # Rows on plane
+    x = 0
+    y = 127
+
+    for c in line[:-4]:
+        if c == 'F':
+            y = math.floor((x + y) / 2)
+        else:
+            x = math.ceil((x + y) / 2)
+
+    if line[6] == 'F':
+        row = x
+    else:
+        row = y
+
+    # Columns on plane
+    a = 0
+    b = 7
+
+    for c in line[-3:]:
+        if c == 'L':
+            b = math.floor((a + b) / 2)
+        else:
+            a = math.ceil((a + b) / 2)
+
+    seat_ids.append(row * 8 + a)  # a and b are always the same
+
+seat_ids.sort()
+
+# Finding my seat
+for i in range(seat_ids[1], seat_ids[-1]):
+    if not i in seat_ids and i + 1 in seat_ids and i - 1 in seat_ids:
+        print(f"The puzzle result is: {i}")
+        break
