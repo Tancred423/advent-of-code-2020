9dbccb7cc791ccb154ac87976f6f5bf767b19ba4
Tancred423 <thonkred@gmail.com>
2020-12-06 13:17:02 +0100

    Day 6

---
+++ d6/d6p2.py
@@ -0,0 +1,28 @@
+lines = open('d6/d6.txt').read().splitlines()
+
+persons = []
+count = 0
+
+
+def checkCount(persons):
+    # Convert group list to one string to a char list to a set
+    # A set won't have duplicates
+    # Then check per char if it's available in all list entries
+    count = 0
+    for char in set(list(''.join(persons))):
+        if all(char in person for person in persons):
+            count += 1
+    return count
+
+
+# Building a list with inputs of one group
+# Then check the count of chars that are available in all list entries
+for line in lines:
+    if not line:
+        count += checkCount(persons)
+        persons.clear()
+        continue
+    persons.append(line)
+count += checkCount(persons)
+
+print(f"The puzzle result is: {count}")
