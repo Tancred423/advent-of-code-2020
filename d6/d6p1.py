5d62dfe19b4c430ccbc77248fb1011cb2ae2c817
Tancred423 <thonkred@gmail.com>
2020-12-06 13:23:35 +0100

    Day 6: Just a bit fancier I suppose

--- d6/d6p1.py
+++ d6/d6p1.py
@@ -2,10 +2,11 @@ lines = open('d6/d6.txt').read().splitlines()
 
 # New input with only one string per group
 inputs = ''.join(['-' if not line else line for line in lines]).split('-')
-sum = 0
 
 # Count length from set from char list from input string
 # A set won't have duplicates, so we get the count of unique chars
+sum = 0
+
 for input in inputs:
     sum += len(set(list(input)))
 
